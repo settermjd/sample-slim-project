82df51e92c00b0c54330314ac4dd049b7ed5517a
Matthew Setter <matthew@matthewsetter.com>
2025-02-06 16:00:52 +1000

    Update the Slim app's configuration
    This change loads environment variables if in development mode and
    explicitly initialises a DI container for the application with a single
    service, named "view", that is a Twig instance.

--- public/index.php
+++ public/index.php
@@ -3,12 +3,28 @@
 declare(strict_types=1);
 
 use App\Handler\DefaultHandler;
+use DI\Container;
 use Slim\Factory\AppFactory;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Slim\Views\Twig;
+use Slim\Views\TwigMiddleware;
 
 require __DIR__ . '/../vendor/autoload.php';
 
+$environment = $_ENV['APP_ENV'] ?? "prod";
+if ($environment === "dev") {
+    $dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . "/../");
+    $dotenv->load();
+}
+
+$container = new Container();
+AppFactory::setContainer($container);
 $app = AppFactory::create();
 
-$app->get('/hello/{name}', DefaultHandler::class);
+$twig = Twig::create(__DIR__ . '/../templates', ['cache' => false]);
+$app->add(TwigMiddleware::create($app, $twig));
+
+$app->get('/', DefaultHandler::class);
 
 $app->run();
